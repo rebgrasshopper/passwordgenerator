ba390a53f3a9841b17e3556530ef0a04a617039e
Plover Brown <rellwoos@gmail.com>
2020-07-15 20:10:31 -0700

    functional password generation js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,36 +1,114 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//PREKNOWN VARIABLES
 
-// Known variables
-  let passwordLength;
-  const OKPASSWORDLENGTH = [(passwordLength > 7), (passwordLength <129)]
+const ALLOWEDSPECIALCHARACTERS = ["!", '"', "#", "$", "%", "&", "\\", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " "];
+
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+const ALLOWEDLOWERCHARACTERS = ALPHABET.split("");
+
+const ALLOWEDUPPERCHARACTERS = ALPHABET.toUpperCase().split("");
+
+const ALLOWEDNUMBERS = '0123456789'.split('');
 
 
 
-// Password Length Options
 
-function chooseLength() {
-  document.getElementById("password").style.display = "none";
-  document.getElementById("option-box").style.display = "block"
-  document.getElementById("password-length-input").style.display = "block";
-  document.getElementById("generate").style.display = "none";
+
+
+//function for checking special characters
+function checkCharacters(array) {
+  let unaccepted = 0;
+  let i = 0;
+
+  while (unaccepted < 1 && i < array.length) {
+    if (ALLOWEDSPECIALCHARACTERS.indexOf(array[i]) === -1) {
+      console.log(array[i] + " is not allowed");
+      unaccepted += 1;
+    } else {
+      console.log(array[i] + " is all good!");
+      i++
+    }
+  }
+  return (unaccepted === 0)
 }
 
-let slider = document.getElementById("passwordLength");
-let output = document.getElementById("sliderValue");
-output.innerHTML = slider.value;
 
-slider.oninput = function () {
-  output.innerHTML = this.value;
+
+
+//function to Generate password
+function generatePassword() {
+  
+  //set password length
+  let passwordLength = parseInt(prompt("How long should your password be? Pick an integer between 8 and 128."));
+  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    passwordLength = prompt("I'm sorry, please input an integer between 8 and 128.");
+  } 
+
+
+  //set special characters
+  let specialCharacters = prompt('Let\'s talk special characters. You can choose any of these, or none!       [space] !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', '!$%@#').split('');
+  while (!checkCharacters(specialCharacters)) {
+    specialCharacters = prompt("I'm sorry, some of those didn't match the allowed characters. You can use  [space] !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', or use these pre-set common characters:", '!$%@#').split('');
+  }
+
+  
+  //prompt Lower, Upper, Numbers
+  let areLowersOK = confirm("Do you want to include lowercase letters?");
+  let areUppersOK = confirm("Do you want uppercase letters?");
+  let areNumbersOK = confirm("Do you want numbers?");
+  
+
+  //total allowed characters
+  let totalAllowedCharacters = specialCharacters;
+
+  if (areLowersOK) {
+    totalAllowedCharacters = totalAllowedCharacters.concat(ALLOWEDLOWERCHARACTERS);
+  }
+  if (areUppersOK) {
+    totalAllowedCharacters = totalAllowedCharacters.concat(ALLOWEDUPPERCHARACTERS);
+  }
+  if (areNumbersOK) {
+    totalAllowedCharacters = totalAllowedCharacters.concat(ALLOWEDNUMBERS);
+  }
+
+
+
+  //let's set things up so there are at least a few symbols
+  const minNumberOfSpecials = Math.ceil(passwordLength/3);
+  let positionsOfSpecials = [];
+  let i=0;
+
+  while (i < minNumberOfSpecials) {
+    newNum = Math.floor(Math.random() * passwordLength);
+    if (positionsOfSpecials.indexOf(newNum) === -1) {
+      positionsOfSpecials.push(newNum);
+      i++;
+    }
+  }
+    
+  
+  //finally build password
+  let passwordText ="";
+  i = 0;
+  while (i < passwordLength) {
+    if (positionsOfSpecials.includes(i)) {
+      let charIndex = Math.floor(Math.random() * specialCharacters.length);
+      passwordText += specialCharacters[charIndex];
+      i++;
+    } else {
+      let charIndex = Math.floor(Math.random() * totalAllowedCharacters.length);
+      passwordText += totalAllowedCharacters[charIndex];
+      i++;
+    }
+  }
+
+  return passwordText //password is made! end of generatePassword()
 }
 
-//Password Character Options 
 
-function chooseCharacters() {
-  document.getElementById("special-characters-input").style.display = "flex";
-  document.getElementById("password-length-input").style.display = "none";
-}
 
 
 
@@ -40,23 +118,13 @@ function chooseCharacters() {
 // Write password to the #password input
 function writePassword() {
 
-  passwordLength = parseInt(prompt("How long do you want your password? Enter an integer between 8 and 128."));
-  
-  
-  while (OKPASSWORDLENGTH) /*ensure correct length */{
-    passwordLength = parseInt(prompt("I'm sorry, you must enter a password length between 8 and 128."));
-  }
+  let password = generatePassword();
+  let passwordHTMLText = document.querySelector("#password");
 
-  
+  passwordHTMLText.value = password;
 
-
-  
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
-
-  // passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", chooseLength);
+generateBtn.addEventListener("click", writePassword);
